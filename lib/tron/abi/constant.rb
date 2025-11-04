acd46da27a5c45ef283d1f149d4dcd00e22da4c8
Bolo Michelin <72155+bolom@users.noreply.github.com>
2025-11-04 10:13:09 -0400

    fix: add missing constants for packed encoder
    - Add UINT_MAX, UINT_MIN, INT_MAX, and INT_MIN constants to Tron::Abi::Constant module
    - These constants are required by the packed encoder for value validation

--- lib/tron/abi/constant.rb
+++ lib/tron/abi/constant.rb
@@ -6,6 +6,22 @@ module Tron
     module Constant
       extend self
 
+      # Maximum value for uint256 (2^256 - 1)
+      # @return [Integer] maximum value for uint256
+      UINT_MAX = 2**256 - 1
+
+      # Minimum value for uint256
+      # @return [Integer] minimum value for uint256
+      UINT_MIN = 0
+
+      # Maximum value for int256 (2^255 - 1)
+      # @return [Integer] maximum value for int256
+      INT_MAX = 2**255 - 1
+
+      # Minimum value for int256 (-(2^255))
+      # @return [Integer] minimum value for int256
+      INT_MIN = -(2**255)
+
       # Byte zero constant
       # @return [String] binary string containing zero byte
       BYTE_ZERO = "\x00".b
